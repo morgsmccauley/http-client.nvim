8a7b782a62258741e535b1be248d94db978f0dc9
Morgan McCauley <morgan@mccauley.co.nz>
2022-09-24 14:07:41 +1200

    refactor: make test inputs inline

--- lua/http-client/parse.lua
+++ lua/http-client/parse.lua
@@ -1,31 +1,39 @@
-local test = {
-  'POST https://postman-echo.com/post',
-  'content-type: application/json',
-  '',
-  '{',
-  '  "name": "morgan",',
-  '  "age": 28',
-  '}',
-  '',
-  '###'
-}
-
 local function parseMethod(line)
   return line:sub(string.find(line, '^%u+'))
 end
 
-print(parseMethod(test[1]))
+print(parseMethod('POST https://postman-echo.com/post'))
 
 local function parseUrl(line)
   local _, finish = string.find(line, '%s+')
   return line:sub(finish + 1)
 end
 
-print(parseUrl(test[1]))
+print(parseUrl('POST https://postman-echo.com/post'))
 
 local function parseHeader(line)
   local separatorStart, separatorFinish = string.find(line, ':%s+')
   return line:sub(0, separatorStart - 1), line:sub(separatorFinish + 1)
 end
 
-print(parseHeader(test[2]))
+print(parseHeader('content-type: application/json'))
+
+local function parseBody(lines)
+  local body = ''
+  for _, line in ipairs(lines) do
+    body = body .. line
+  end
+  return vim.fn.json_decode(body)
+end
+
+vim.pretty_print(parseBody({
+  '{',
+  '  "name": "morgan",',
+  '  "age": 28,',
+  '  "address": {',
+  '    "number": 123,',
+  '    "street": "fake st",',
+  '    "city": "california"',
+  '  }',
+  '}',
+}))
