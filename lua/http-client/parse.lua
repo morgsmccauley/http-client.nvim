d23784d3ce7fd2d7a324a9560c79bdad7de653ae
Morgan McCauley <morgan@mccauley.co.nz>
2022-09-26 13:50:43 +1300

    refactor: export parse functions from module

--- lua/http-client/parse.lua
+++ lua/http-client/parse.lua
@@ -1,24 +1,20 @@
-local function parseMethod(line)
+local M = {}
+
+function M.method(line)
   return line:sub(string.find(line, '^%u+'))
 end
 
-print(parseMethod('POST https://postman-echo.com/post'))
-
-local function parseUrl(line)
+function M.url(line)
   local _, finish = string.find(line, '%s+')
   return line:sub(finish + 1)
 end
 
-print(parseUrl('POST https://postman-echo.com/post'))
-
-local function parseHeader(line)
+function M.header(line)
   local separatorStart, separatorFinish = string.find(line, ':%s+')
   return line:sub(0, separatorStart - 1), line:sub(separatorFinish + 1)
 end
 
-print(parseHeader('content-type: application/json'))
-
-local function parseBody(lines)
+function M.body(lines)
   local body = ''
   for _, line in ipairs(lines) do
     body = body .. line
@@ -26,33 +22,21 @@ local function parseBody(lines)
   return vim.fn.json_decode(body)
 end
 
-vim.pretty_print(parseBody({
-  '{',
-  '  "name": "morgan",',
-  '  "age": 28,',
-  '  "address": {',
-  '    "number": 123,',
-  '    "street": "fake st",',
-  '    "city": "california"',
-  '  }',
-  '}',
-}))
-
-local function parseRequest(lines)
+function M.request(lines)
   local headers = {}
   local body = nil
 
   local requestLine = table.remove(lines, 1)
-  local method = parseMethod(requestLine)
-  local url = parseUrl(requestLine)
+  local method = M.method(requestLine)
+  local url = M.url(requestLine)
 
   repeat
     local line = table.remove(lines, 1)
-    local header, value = parseHeader(line)
+    local header, value = M.header(line)
     headers[header] = value
   until lines[1] == '' or lines[1] == nil
 
-  local line = table.remove(lines, 1)
+  table.remove(lines, 1)
 
   if lines[1] == '{' then
     local jsonString = ''
@@ -66,18 +50,4 @@ local function parseRequest(lines)
   return method, url, headers, body
 end
 
-vim.pretty_print(parseRequest({
-  'POST https://postman-echo.com/post',
-  'content-type: application/json',
-  'origin: neovim',
-  '',
-  '{',
-  '  "name": "morgan",',
-  '  "age": 28,',
-  '  "address": {',
-  '    "number": 123,',
-  '    "street": "fake st",',
-  '    "city": "california"',
-  '  }',
-  '}',
-}))
+return M
